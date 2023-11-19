37d1ab58d4ba5818fe3863babf7b35a98f3608cc
Udayan <udhayan.addon@gmail.com>
2023-11-19 11:10:26 +0530

    Initial

---
+++ src/test/java/com/fluorscent/sampleapp/SampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluorscent.sampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
